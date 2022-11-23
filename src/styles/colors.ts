d9dc7f9d78d3e8724140355e392e7fe90c52f3ee
Nikodem Wrona <wronanikodem@protonmail.com>
2022-11-24 00:39:47 +0100

    feat: update colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,12 +1,12 @@
 export const TEXT_PRIMARY = '#ffffff';
-export const TEXT_SECONDARY = '#141414';
+export const TEXT_SECONDARY = '#1D1B1B';
 export const TEXT_FADED = '#757575';
-export const TEXT_BRAND = '#2CE8C5'
+export const TEXT_BRAND = '#EC4D37'
 
-export const BACKGROUND_PRIMARY = '#141414';
+export const BACKGROUND_PRIMARY = '#1D1B1B';
 export const BACKGROUND_SECONDARY = '#E8E8E8';
 export const BACKGROUND_FADED = '#757575';
 
-export const BACKGROUND_BRAND = '#2CE8C5';
-export const BACKGROUND_BRAND_HIGHLIGHTED = 'rgba(44, 232, 197, .8)';
-export const BACKGROUND_BRAND_FADED = 'rgba(44, 232, 197, .5)';
+export const BACKGROUND_BRAND = '#EC4D37';
+export const BACKGROUND_BRAND_HIGHLIGHTED = 'rgba(236, 77, 55, .8)';
+export const BACKGROUND_BRAND_FADED = 'rgba(236, 77, 55, .5)';
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     font-size: 10px;
     font-family: 'Roboto Mono', monospace;
+    letter-spacing: .1rem;
   }
 
   a {
