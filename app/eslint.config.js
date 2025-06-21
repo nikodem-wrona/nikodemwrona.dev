5436f9e655f0e4f6c89abca956344c2dc5d2ad3f
Nikodem Wrona <wronanikodem@gmail.com>
2025-06-21 21:30:19 +0200

    Sets up base React application structure
    Restructures the project to house the React application in an 'app' directory.
    
    Configures ESLint with import sorting rules and Prettier for code formatting.
    Adds necessary scripts for linting and formatting.
    
    Updates the HTML title and adjusts dependencies for the React app.
    
    Adds a basic component and styling.

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,6 +17,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +25,8 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
-  },
-)
+  }
+);
